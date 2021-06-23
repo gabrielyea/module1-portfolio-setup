dc7a94ce41b2b9fe0281a272b65f6ef642c77c2e
Gabriel <gabriel.gabrielyea@gmail.com>
2021-06-23 16:59:34 -0500

    Add links to the projects

--- scripts/projectClass.js
+++ scripts/projectClass.js
@@ -43,8 +43,8 @@ export const projectsData = [
       'ruby',
       'html',
     ],
-    'link1',
-    'link2'),
+    'https://longdogechallenge.com/',
+    'https://alwaysjudgeabookbyitscover.com/'),
   new Project('1',
     'Project 2',
     'Sometimes I stare at a door or a wall and I wonder what is this reality, why am I alive, and what is this all about?.',
@@ -53,8 +53,8 @@ export const projectsData = [
       'ruby',
       'javascript',
     ],
-    'link1',
-    'link2'),
+    'http://eelslap.com/',
+    'http://www.staggeringbeauty.com/'),
   new Project('2',
     'Project 3',
     'Its much more difficult to play tennis with a bowling ball than it is to bowl with a tennis ball.',
@@ -64,8 +64,8 @@ export const projectsData = [
       'javascript',
       'ruby',
     ],
-    'link1',
-    'link2'),
+    'http://corndog.io/',
+    'https://smashthewalls.com/'),
   new Project('3',
     'Project 4',
     'Culpa velit anim dolor cupidatat qui fugiat commodo commodo adipisicing ad ut ut eiusmod nisi. Quis exercitation amet ex exercitation velit qui est deserunt. Nisi elit quis commodo do veniam.',
@@ -74,8 +74,8 @@ export const projectsData = [
       'html',
       'javascript',
     ],
-    'link1',
-    'link2'),
+    'https://jacksonpollock.org/',
+    'http://www.republiquedesmangues.fr/'),
   new Project('4',
     'Project 5',
     'He was so preoccupied with whether or not he could thathe failed to stop to consider if he should. He took one look at what was under the table and noped the hell out of there.',
@@ -85,8 +85,8 @@ export const projectsData = [
       'javascript',
       'Bootstrap',
     ],
-    'link1',
-    'link2'),
+    'http://corndog.io/',
+    'https://longdogechallenge.com/'),
   new Project('5',
     'Project 6',
     'Excepteur elit consequat minim ea consequat cillum irure cillum nisi elit eiusmod. Non eiusmod officia culpa culpa nulla mollit occaecat ut cupidatat amet exercitation nostrud labore dolor. Lorem laborum reprehenderit esse do anim.',
@@ -95,8 +95,8 @@ export const projectsData = [
       'html',
       'Bootstrap',
     ],
-    'link1',
-    'link2'),
+    'http://www.staggeringbeauty.com/',
+    'https://smashthewalls.com/'),
   new Project('6',
     'Project 7',
     'He was so preoccupied with whether or not he could thathe failed to stop to consider if he should. He took one look at what was under the table and noped the hell out of there.',
@@ -105,8 +105,8 @@ export const projectsData = [
       'javascript',
       'Bootstrap',
     ],
-    'link1',
-    'link2'),
+    'https://alwaysjudgeabookbyitscover.com/',
+    'http://corndog.io/'),
 ];
 
 const createTech = (techName) => {
@@ -116,6 +116,8 @@ const createTech = (techName) => {
   return newLi;
 };
 
+// This should also work in all projecs types...
+// will try to fix this later...
 export const setProjectData = (emptyProject, projectIndex) => {
   emptyProject.style.backgroundImage = `url(${projectsData[projectIndex].projectImage})`;
   emptyProject.querySelector('#project-title').innerText = projectsData[projectIndex].name;
@@ -127,6 +129,10 @@ export const setProjectData = (emptyProject, projectIndex) => {
   return emptyProject;
 };
 
+// TODO: due to your poor design choises we find this situation!
+// >.< find a way to fix this!!!
+// I think I dont need the selected project, I can find project by name
+// will fix later!!
 export const displayPopProject = (popWindowRef, selectedProject) => {
   const pTitle = selectedProject.querySelector('#project-title').innerText;
   const currentPro = projectsData.find((n) => n.name === pTitle);
@@ -137,6 +143,8 @@ export const displayPopProject = (popWindowRef, selectedProject) => {
   currentPro.technologies.forEach((tech) => {
     popWindowRef.addTech(createTech(tech));
   });
+  document.querySelector('#live-link').setAttribute('href', currentPro.liveLink);
+  document.querySelector('#source-link').setAttribute('href', currentPro.sourceLink);
 };
 
 export const displaySpecialProject = (popWindowRef) => {
@@ -149,4 +157,6 @@ export const displaySpecialProject = (popWindowRef) => {
   selectedProject.technologies.forEach((tech) => {
     popWindowRef.addTech(createTech(tech));
   });
+  document.querySelector('#live-link').setAttribute('href', selectedProject.liveLink);
+  document.querySelector('#source-link').setAttribute('href', selectedProject.sourceLink);
 };
